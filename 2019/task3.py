919367f77ba8d0a544fce3e5eb7227978c66f39a
wernad <jztnem@gmail.com>
2021-01-26 22:06:25 +0100

    New repo

---
+++ 2019/task3.py
@@ -0,0 +1,88 @@
+import re
+from itertools import tee
+import more_itertools
+import matplotlib.pyplot as plt
+
+with open('input') as file:
+    program_input = file.read()
+    wire1_steps, wire2_steps = program_input.split('\n')
+    wire1_steps = [(x[0], int(x[1:])) for x in wire1_steps.replace(',',' ').split()]
+    wire2_steps = [(x[0], int(x[1:])) for x in wire2_steps.replace(',',' ').split()]
+    
+def draw_wire(wire_steps):
+    coords = [[[0,0], 0]] #[[x,y], distance]
+    for x in wire_steps:
+        new_coord = [[coords[-1][0][0], coords[-1][0][1]],coords[-1][1]]
+        if x[0] == 'L':
+            new_coord[0][0] -= x[1]
+        elif x[0] == 'R':
+            new_coord[0][0] += x[1]
+        elif x[0] == 'U':
+            new_coord[0][1] += x[1]
+        elif x[0] == 'D':
+            new_coord[0][1] -= x[1]
+        new_coord[1] += x[1]
+        coords.append(new_coord)
+    return coords
+
+#determinant
+def det(a, b):
+    return a[0] * b[1] - a[1] * b[0]
+
+def pairwise(iterable):
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+# 0 -> horizontal
+# 1 -> vertical
+def get_line_type(a,b):
+    if a[0] == b[0]:
+        return 1
+    else:
+        return 0
+
+def find_intersections(wire1, wire2):
+    intersections = []
+    distances = []
+    for a1, a2 in pairwise(wire1):
+        a1_coords = a1[0]
+        a2_coords = a2[0]
+        wire1_type = get_line_type(a1_coords,a2_coords)
+        
+        
+        for b1, b2 in pairwise(wire2):
+            b1_coords = b1[0]
+            b2_coords = b2[0]
+            wire2_type = get_line_type(b1_coords, b2_coords)
+
+            if wire1_type == wire2_type: #paralel lines
+                pass
+            elif wire1_type == 1: 
+                if ((b1_coords[0] < a1_coords[0] < b2_coords[0]) or (b2_coords[0] < a1_coords[0] < b1_coords[0])) and ((a1_coords[1] < b1_coords[1] < a2_coords[1]) or (a2_coords[1] < b1_coords[1] < a1_coords[1])):
+                    intersections.append((a1_coords[0], b1_coords[1]))
+                    distances.append(a1[1] + b1[1] + abs(a1_coords[0] - b1_coords[0]) + abs(a1_coords[1] - b1_coords[1])) #part 2
+            else:
+                if ((a1_coords[0] < b1_coords[0] < a2_coords[0]) or (a2_coords[0] < b1_coords[0] < a1_coords[0])) and ((b1_coords[1] < a1_coords[1] < b2_coords[1]) or (b2_coords[1] < a1_coords[1] < b1_coords[1])):
+                    intersections.append((b1_coords[0], a1_coords[1]))
+                    distances.append(a1[1] + b1[1] + abs(a1_coords[0] - b1_coords[0]) + abs(a1_coords[1] - b1_coords[1])) #part 2
+    return intersections, distances
+
+#part 1
+def find_shortest_md(intersections):
+    distances = []
+
+    def manhattan_distance(p1, p2):
+        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+    for x in intersections:
+        distances.append(manhattan_distance((0,0), x))
+    return min(distances)
+
+
+wire1_coords = draw_wire(wire1_steps)
+wire2_coords = draw_wire(wire2_steps)
+
+intersections, distances = find_intersections(wire1_coords, wire2_coords)
+print('Manhattan distance:', find_shortest_md(intersections))
+print('Distance via wire:', min(distances))
---
+++ 2019/task4.py
@@ -0,0 +1,48 @@
+from itertools import tee
+import more_itertools
+import copy
+
+'''
+def pairwise(iterable):
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+'''
+
+def tripletwise(iterable):
+    a, b = tee(iterable)
+    next(b, None)
+    c = copy.copy(b)
+    next(c, None)
+    return zip(a,b,c)
+    
+
+count = 0
+isolated_count = 0
+for i in range(357253, 892942):
+    double_found = 0
+    isolated_double_found = 0    
+    incremental = 1
+    last_a = -1
+    for a, b, c in tripletwise(str(i)):
+        if int(a) > int(b) or int(b) > int(c):
+            incremental = 0
+            break
+        if a == b or b == c: #part 1
+            double_found = 1
+        if a == b == c: #part 2
+            pass
+        elif a == b and a != last_a:
+            isolated_double_found = 1
+        last_a = a
+    else:
+        if a != b and b == c:
+            isolated_double_found = 1
+    if double_found == 1 and incremental == 1:
+        count += 1
+    if isolated_double_found == 1 and incremental == 1:
+        isolated_count += 1
+
+print(count)
+
+print(isolated_count)
