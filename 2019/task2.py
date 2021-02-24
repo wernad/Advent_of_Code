2fa3dc8eba62d266a4d3975f4c3ce2b9b0b04478
wernad <jztnem@gmail.com>
2021-02-25 00:25:04 +0100

    Refactorization

---
+++ 2019/task2.py
@@ -0,0 +1,28 @@
+import intcode_computer as ic
+
+puzzle_input = ic.load_input('2')
+computer = ic.IntcodeComputer(puzzle_input.copy(), False)
+
+#Part 1
+computer.process_intcode()
+print('Part 1:', computer.get_puzzle_input()[0])
+#Part 2
+found = 0
+i = 0
+j = 0
+while j < 100:
+    puzzle_input[1] = i
+    puzzle_input[2] = j
+
+    computer.set_puzzle_input(puzzle_input.copy())
+    computer.set_opcode_position(0)
+    computer.process_intcode()
+    if computer.get_puzzle_input()[0] == 19690720:
+        break
+
+    i = i + 1
+    if i > 99:
+        i = 0
+        j = j + 1
+
+print('Part 2:', 100*i+j)
