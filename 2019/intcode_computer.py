d6dcfbcdde44ccacb5ced64f59f811a6c77656e5
wernad <jztnem@gmail.com>
2021-02-28 03:01:19 +0100

    Day 7 done

--- 2019/intcode_computer.py
+++ 2019/intcode_computer.py
@@ -1,7 +1,7 @@
 def load_input(task_number):
     with open('puzzle_input/input' + task_number +'.txt') as file:
         puzzle_input = file.read()
-        puzzle_input = puzzle_input.replace(',', ' ').split()
+        puzzle_input = puzzle_input.split(',')
         puzzle_input = [int(i) for i in puzzle_input]
     return puzzle_input
 
@@ -12,14 +12,14 @@ class IntcodeComputer:
         self.puzzle_input_copy = puzzle_input.copy()
         self.opcode_position = 0
         self.feedback = feedback
-        self.input = None
-        self.output = None
+        self.input = []
+        self.output = []
 
-    def restart(self):
-        self.puzzle_input = self.puzzle_input_copy.copy()
+    def restart(self, puzzle_input = None):
+        self.puzzle_input = self.puzzle_input_copy.copy() if puzzle_input is None else puzzle_input
         self.opcode_position = 0
-        self.input = None
-        self.output = None
+        self.input = []
+        return self
 
     def set_puzzle_input(self, puzzle_input):
         self.puzzle_input = puzzle_input.copy()
@@ -27,8 +27,10 @@ class IntcodeComputer:
     def set_opcode_position(self, pos):
         self.opcode_position = pos
 
-    def set_input(self, program_input):
-        self.input = program_input
+    def set_input(self, *program_input):
+        self.input = []
+        for x in program_input:
+            self.input.append(x)
     
     def get_output(self):
         return self.output
@@ -41,7 +43,7 @@ class IntcodeComputer:
             if param_mode == 0:
                 return self.puzzle_input[value]
             return value
-                    
+
         self.output = []
         i = self.opcode_position
         while True:
@@ -50,34 +52,35 @@ class IntcodeComputer:
             opcode = int(opcode[-2:])
             if opcode == 99:
                 if self.feedback:
-                    return
-                break
+                    output = None
+                return
             elif opcode in {3, 4}:
-                self.opcode_position = i
-                if opcode == 3:
+                if opcode == 3: #input
                     self.puzzle_input[self.puzzle_input[i+1]] = self.input.pop(0) if len(self.input) > 1 else self.input[0]
-                elif opcode == 4:
+                elif opcode == 4: #output
                     param_1 = __get_param(param_modes[-1], self.puzzle_input[i+1])
                     self.output.append(param_1)
                     if self.feedback:
+                        i += 2
+                        self.opcode_position = i
                         return
                 i += 2
             elif opcode in {1, 2, 5, 6, 7, 8}:
                 param_1 = __get_param(param_modes[-1], self.puzzle_input[i+1])
                 param_2 = __get_param(param_modes[-2], self.puzzle_input[i+2])
-                if opcode == 1:
+                if opcode == 1: # a + b
                     self.puzzle_input[self.puzzle_input[i+3]] = param_1 + param_2
-                elif opcode == 2:
+                elif opcode == 2: # a * b
                     self.puzzle_input[self.puzzle_input[i+3]] = param_1 * param_2
-                elif opcode == 5:
+                elif opcode == 5:# jump if not equal to 0
                     i = param_2 if param_1 != 0 else i+3
                     continue
-                elif opcode == 6:
+                elif opcode == 6: # jump if equal to 0
                     i = param_2 if param_1 == 0 else i+3
                     continue
-                elif opcode == 7:
+                elif opcode == 7: # 1 if a < b else 0
                     self.puzzle_input[self.puzzle_input[i+3]] = 1 if param_1 < param_2 else 0
-                elif opcode == 8:
+                elif opcode == 8: # 1 if a == b else 0
                     self.puzzle_input[self.puzzle_input[i+3]] = 1 if param_1 == param_2 else 0
                 i += 4
         
---
+++ 2019/task7.py
@@ -0,0 +1,51 @@
+import intcode_computer as ic
+
+from itertools import permutations 
+import time
+puzzle_input = ic.load_input('7')
+
+
+start_time = time.time()
+
+
+def max_signal(puzzle_input):
+    phases = permutations([0, 1, 2, 3, 4])
+    amp = ic.IntcodeComputer(puzzle_input.copy(), False)
+    values = []
+    for phase in phases:    
+        last_output = 0
+        for i in range(5):
+            amp.set_input(phase[i], last_output)
+            amp.process_intcode()
+            last_output = amp.get_output()[-1]
+            amp.restart()
+        values.append(last_output)
+    return max(values)
+
+def max_signal_feedback(puzzle_input):
+    phases = permutations([5, 6, 7, 8, 9])
+    amps = [ic.IntcodeComputer(puzzle_input.copy(), True) for i in range(5)]
+    values = []
+    for phase in phases:
+        amps = [amp.restart() for amp in amps]
+        last_output = 0
+        finished = False
+        for i in range(5):
+            amps[i].set_input(phase[i], last_output)
+            amps[i].process_intcode()
+            last_output = amps[i].get_output()[-1]
+        while not finished:
+            for i in range(5):
+                amps[i].set_input(last_output)
+                amps[i].process_intcode()
+                if amps[i].get_output():
+                    last_output = amps[i].get_output()[-1]
+                else:
+                    finished = True
+                    break
+        values.append(last_output)
+    return max(values)
+            
+print('Part 1:', max_signal(puzzle_input))
+print('Part 2:', max_signal_feedback(puzzle_input))
+print("--- %s seconds ---" % (time.time() - start_time))
