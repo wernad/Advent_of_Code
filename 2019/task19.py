03bfdd589c7eb05dbd7e03d180aedc74a3d81b95
wernad <jztnem@gmail.com>
2021-04-15 21:05:32 +0200

    small optimalization

--- 2019/task19.py
+++ 2019/task19.py
@@ -31,6 +31,7 @@ def find_closest_point(square_side):
     x_min = 0
     x_max = 3
     for y in count(start=0):
+        print(y, x_min, x_max)
         for x in range(x_min, x_max):   
             drone.input = [x, y]
             drone.process_intcode()
@@ -38,7 +39,7 @@ def find_closest_point(square_side):
             drone.restart()
             if output == 0:
                 if last == '#':
-                    x_max += 1
+                    x_max = x + 3
                     break
                 last = '.'
             else:
