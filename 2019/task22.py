524097c3aa940227d9d99b7b701f0005b6a3bbaf
wernad <jztnem@gmail.com>
2021-04-24 20:41:57 +0200

    Day 22 part 2 done

--- 2019/task22.py
+++ 2019/task22.py
@@ -23,7 +23,58 @@ def shuffle(deck):
             deck = list(reversed(deck))
     return deck
 
+def shuffle_lcf(deck_size):
+    global puzzle_input
+    a,b = 1,0
+    for technique in puzzle_input:
+        func, param = re.search(r'((?:[a-z]+ ?)+)(-?\d+)?', technique).groups()
+        func = re.sub(r'\s$', '', func)
+        if func == 'cut':
+            param = int(param)
+            new_a, new_b = 1, -param
+        elif func == 'deal with increment':
+            param = int(param)
+            new_a, new_b = param, 0
+        else: #into stack
+            new_a, new_b = -1, -1
+        a = new_a * a % deck_size
+        b = (new_a * b + new_b) % deck_size
+    return a,b
+
+#Compose g o f
+def pow_mod(x, n, m):
+    y = 1
+    while n > 0:
+        if n % 2 != 0:
+            y = y * x % m
+        x = x * x % m
+        n //= 2
+    return y
+    
+def pow_compose(f, k, m):
+    def compose(f, g, m):
+        a,b = f
+        c,d = g
+        return (a * c % m, (b * c + d) % m)
+
+    g = (1,0)
+    while k > 0:
+        if k % 2 != 0:
+            g = compose(g, f, m)
+        f = compose(f,f, m)
+        k //= 2
+    return g
+
 deck_size = 10007
 deck = list(range(deck_size))
 
+deck_size = 119315717514047
+repeat = 101741582076661
+
+a, b =  shuffle_lcf(deck_size)
+
+f = (a,b)
+A, B = pow_compose(f, repeat, deck_size)
+
 print('Part 1:', shuffle(deck).index(2019))
+print('Part 2:', (2020 - B) * pow_mod(A, deck_size - 2, deck_size) % deck_size)
