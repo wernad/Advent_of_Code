7d8a210887f51525fa83fd3c64af696e4f2beb5c
wernad <jztnem@gmail.com>
2021-05-06 17:19:47 +0200

    Day 24 done

--- 2019/task24.py
+++ 2019/task24.py
@@ -40,23 +40,26 @@ def calc_infestation(initial_layout):
     bottom_center = (2,3)
 
     outer2inner = {
-        (1,2) : list(zip([0]*5, range(5))),
-        (3,2) : list(zip([4]*5, range(5))),
-        (2,1) : list(zip(range(5), [0]*5)),
-        (2,3) : list(zip(range(5), [4]*5))
+        left_center : list(zip([0]*5, range(5))),
+        right_center : list(zip([4]*5, range(5))),
+        top_center : list(zip(range(5), [0]*5)),
+        bottom_center : list(zip(range(5), [4]*5))
     }
 
     global directions, square_side
-    layers = [initial_layout]
+    layers = {0: initial_layout}
 
-    for _ in range(10):
-        if layers[0]:
-            layers.insert(0, [])
-        if layers[-1]:
-            layers.insert(-1, [])
+    for _ in range(200):
+        min_level, max_level = min(layers.keys()), max(layers.keys())
+        if layers[min_level]:
+            layers[min_level - 1] = []
+            min_level -= 1
+        if layers[max_level]:
+            layers[max_level + 1] = []
+            max_level += 1
         
-        new_layers = []
-        for index, layout in enumerate(layers):
+        new_layers = dict()
+        for level, layout in layers.items():
             #print(index)
             new_layout = []
             for y in range(square_side):
@@ -70,29 +73,29 @@ def calc_infestation(initial_layout):
                         if neighbour in layout and neighbour != center:
                             neighbourhood += 1
                             
-                    if index != 0:
-                        if x == 0 and left_center in layers[index - 1]:
+                    if level != min_level:
+                        if x == 0 and left_center in layers[level - 1]:
                             neighbourhood += 1
-                        elif x == 4 and right_center in layers[index - 1]:
+                        elif x == 4 and right_center in layers[level - 1]:
                             neighbourhood += 1
-                        if y == 0 and top_center in layers[index - 1]:
+                        if y == 0 and top_center in layers[level - 1]:
                             neighbourhood += 1
-                        elif y == 4 and bottom_center in layers[index - 1]:
+                        elif y == 4 and bottom_center in layers[level - 1]:
                             neighbourhood += 1
 
-                    if (x,y) in {(1,2), (3,2), (2,1), (2,3)} and index != len(layers) - 1:
+                    if (x,y) in {(1,2), (3,2), (2,1), (2,3)} and level != max_level:
                         for inner_neighbour in outer2inner[(x,y)]:
-                            if inner_neighbour in layers[index + 1]:
+                            if inner_neighbour in layers[level + 1]:
                                 neighbourhood += 1
 
                     if ((x,y) in layout and neighbourhood == 1) or ((x,y) not in layout and neighbourhood in {1,2}):
                         new_layout.append((x,y))            
-            new_layers.append(new_layout)
+            new_layers[level] = new_layout
         layers = new_layers
-    for x in layers:
-        print(len(x), x)
+    for k,v in layers.items():
+        print(k, len(v))
     print('Total layers:',len(layers))
-    return sum([len(layout) for layout in layers])
+    return sum([len(layout) for layout in layers.values()])
 
 
 directions = [(1,0), (-1,0), (0,1), (0,-1)]
@@ -110,7 +113,7 @@ powers_of_two = []
 for y in range(5):
     for x in range(5):
         if (x,y) in first_duplicate:
-            powers_of_two.append(2**(x+y*5))
+            powers_of_two.append(2**(x+y*square_side))
 
 print('Part 1:', sum(powers_of_two))
 print('Part 2: ', calc_infestation(initial_layout))
